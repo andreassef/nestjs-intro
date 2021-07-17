c0b3d91782efcd1a705435e564c4242f068bbf0a
andreassef <andreassef02@gmail.com>
2021-07-16 20:26:33 -0400

    [UPDATE] - Conectando com banco de dados

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,28 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { SequelizeModule } from '@nestjs/sequelize';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { BooksController } from './book.controller';
+import { Book } from './Book.model';
+import { BookService } from './Products.service';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot(),
+    SequelizeModule.forRoot({
+      dialect: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'root',
+      database: 'livraria',
+      autoLoadModels: true,
+      synchronize: true,
+    }),
+    SequelizeModule.forFeature([Book]),
+  ],
+  controllers: [AppController, BooksController],
+  providers: [AppService, BookService],
 })
 export class AppModule {}
